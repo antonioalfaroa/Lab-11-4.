7371bc0f8d6054964f514b6ab02f92a6cf6b9f5f
Antonio <78964462+antonioalfaroa@users.noreply.github.com>
2022-08-10 06:09:45 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+
+  int x;
+  string word;
+  cout<<"Enter a word : "<<endl;
+  cin>>word;
+  cout<<"The string length is : "<<word.length()<<endl;
+  int neword = word.length()*2;
+  cout<<"The string length *2 is : "<<setw(word.length()*2)<<neword<<endl;
+  return 0;
+}
